980714729e8b15aa9dba3ef1169e2e978a96ee23
M7Development <mateusz.munacoha@gmail.com>
2023-02-07 23:40:51 +0100

    loops zwischenstand

--- pyLoops.py
+++ pyLoops.py
@@ -1,5 +1,28 @@
+## WhileLoop
 spam = 0
 while spam < 5:
     print('Hello World')
     spam = spam + 1
-    
+
+name = ''
+while name != 'your name':
+    print('Please type your name')
+    name = input()
+
+spam = 0
+while spam < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print('spam is ' + str(spam))
+
+## ForLoop
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+print('-------------------------------- Gaus --------------------------')
+sum = 0
+for i in range(101):
+    sum=i+sum
+print('Das Ergebnis ist ' + str(sum))
